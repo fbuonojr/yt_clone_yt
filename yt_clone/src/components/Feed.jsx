246835d779fae057cce4187f2746355eb606a981
fbuonojr <fbuonojr@fordham.edu>
2023-06-28 23:51:38 -0400

    fixing issues

--- yt_clone/src/components/Feed.jsx
+++ yt_clone/src/components/Feed.jsx
@@ -1,38 +1,38 @@
-import { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import { SideBar, Videos } from './';
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos, SideBar } from "./";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
+    }, [selectedCategory]);
+
   return (
-    <Stack 
-      sx={{ flexDirection: { sx: "column", md: "row"}}}    
-    >
-      <Box 
-        sx={{ height: { sx:'auto', md:'92vh' }, borderRight:'1px solid #3d3d3d', px:{ sx:0, md: 2 } }}
-      >
-        <SideBar />
-        <Typography 
-          className="copyright" 
-          variant="body2" 
-          sx={{ mt: 1.5,color: '#fff' }}
-          >
-            Copyright 2023
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+        <SideBar 
+          selectedCategory={selectedCategory} 
+          setSelectedCategory={setSelectedCategory} 
+        />
+        
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Copyright © 2022 JSM Media
         </Typography>
       </Box>
 
-      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
-        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ 
-          color: 'white'
-        }}>
-          New <span style={{ color: '#f31503'}}
-          >
-            videos
-          </span>
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+          {selectedCategory} <span style={{ color: "#FC1503" }}>videos</span>
         </Typography>
-        <Videos videos={[]} />
+
+        <Videos />
       </Box>
     </Stack>
-  )
-}
+  );
+};
 
-export default Feed
+export default Feed;
