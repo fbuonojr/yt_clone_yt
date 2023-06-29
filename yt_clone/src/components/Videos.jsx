ca597e392266884299b0a5e841bf980d12d69d47
fbuonojr <fbuonojr@fordham.edu>
2023-06-29 19:43:15 -0400

    video card

--- yt_clone/src/components/Videos.jsx
+++ yt_clone/src/components/Videos.jsx
@@ -1,16 +1,21 @@
-import { Stack, Box } from '@mui/material';
+import React from "react";
+import { Stack, Box } from "@mui/material";
 
-const Videos = ({ videos }) => {
+import { ChannelCard, VideoCard } from "./";
+
+const Videos = ({ videos, direction }) => {
+  // if(!videos?.length) return <Loader />;
+  
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
-      {videos.map((item, index) => {
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+      {videos.map((item, idx) => (
         <Box key={idx}>
-          {item.id.videoId && <VideoCard video={item} />}
+          {item.id.videoId && <VideoCard video={item} /> }
           {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
-      })}
+      ))}
     </Stack>
-  )
+  );
 }
 
 export default Videos;
