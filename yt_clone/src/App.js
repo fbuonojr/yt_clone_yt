b5b423d406395eabf5e79e9ee6b01008a10d524f
fbuonojr <fbuonojr@fordham.edu>
2023-06-29 21:17:15 -0400

    channel detail page

--- yt_clone/src/App.js
+++ yt_clone/src/App.js
@@ -7,10 +7,14 @@ const App = () => (
     <Box sx={{ backgroundColor: '#000' }}>
       <Navbar />
       <Routes>
-        <Route exact path='/' element={<Feed />} />
-        <Route path='/video/:id' element={<VideoDetail />} />
-        <Route path='/channel/:id' element={<ChannelDetail />} />
-        <Route path='/search/:searchTerm' element={<SearchFeed />} />
+        <Route exact path='/' 
+        element={<Feed />} />
+        <Route path='/video/:id' 
+        element={<VideoDetail />} />
+        <Route path='/channel/:id' 
+        element={<ChannelDetail />} />
+        <Route path='/search/:searchTerm' 
+        element={<SearchFeed />} />
       </Routes>
     </Box>
   </BrowserRouter>
